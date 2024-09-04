3ed615de0767a286d589ee9a6c15fe1c0fcf1258
Dianitaginting <dianitaginting06@gmail.com>
2024-09-04 21:27:12 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24044 - DIANITA LORENSIA BR GINTING
+// 12S24044 - DIANITA LORENSIA BR GINTINGimport java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
+
 
